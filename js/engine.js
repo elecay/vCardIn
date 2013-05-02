72f678b68079017a068774bb76a08ea50e09175d
elecay <elecay@gmail.com>
2013-05-02 03:44:19 +0200

    All basic info added

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,7 @@
 
+    contactsImported = 0;
+    contactsError = 0;
+
     var storage = navigator.getDeviceStorage("sdcard"), 
     cursor = storage.get('contacts.vcf'); 
 
@@ -12,33 +15,110 @@
       oFReader.readAsText(file);
 
       oFReader.onload = function (oFREvent) {
-        arr = vCard.initialize(oFREvent.target.result);
+        var arr = vCard.initialize(oFREvent.target.result);
 
         for (var i = 0; i < arr.length; i++){
           var contact = new mozContact();
-          person = arr[i];
+          var person = arr[i];
+
+          var emails = [];
+          var addresses = [];
+          var tels = [];
+          var urls = [];
+
+          for(type in person.email) {
+            for(n in person.email[type]) {
+              value = person.email[type][n];
+              email = { type: type, value: value };
+              emails.push(email);
+            }
+          }
+
+          for(type in person.tel) {
+            for(n in person.tel[type]) {
+              value = person.tel[type][n];
+              tel = { type: type, value: value };
+              tels.push(tel);
+            }
+          }
+          /*
+          * According to RFC-2426 (http://www.ietf.org/rfc/rfc2426.txt)
+          */
+          var POST_OFFICE_BOX = 0;
+          var EXTENDED_ADDRESS = 1;
+          var STREET_ADDRESS = 2;
+          var LOCALITY = 3;
+          var REGION = 4;
+          var POSTAL_CODE = 5;
+          var COUNTRY_NAME = 6;
+
+          for(type in person.adr) {
+            for(n in person.adr[type]) {
+              value = person.adr[type][n];
+              address = {
+                type: type,
+                streetAddress: value[STREET_ADDRESS],
+                locality: value[LOCALITY],
+                region: value[REGION],
+                postalCode: value[POSTAL_CODE],
+                countryName: value[COUNTRY_NAME]
+              };
+              addresses.push(address);
+            }
+          }
+
+          for(type in person.url) {
+            for(n in person.url[type]) {
+              value = person.url[type][n];
+              url = { type: type, value: value };
+              urls.push(url);
+            }
+          }
+
+          // leaving just the last value as last name by default
+          names = person.fn.split(' ');
+          n = "";
+          ln = "";
+          if (names.length > 1) {
+            for (var j = 0; j < names.length - 1; j++){
+              n += names[i] + " ";
+            }
+            ln = names[names.length - 1];
+          } else {
+            n = names[0] + " ";
+          }
 
           contact.init(
             {
               name: person.fn,
-              givenName: person.fn.split(' ')[0],
-              familyName: person.fn.split(' ')[1],
-              email: { type: 'INTERNET', value: 'elecay@gmail.com'}
+              givenName: n.substring(0, n.length - 1),
+              familyName: ln,
+              email: emails,
+              url: urls,
+              tel: tels,
+              adr: addresses,
+              org: person.org,
+              photo: person.photo,
+              jobTitle: person.title,
+              bday: person.bday
             }
           );
+
           var request = navigator.mozContacts.save(contact);
 
           request.onsuccess = function onsuccess() {
+            contactsImported++;
             console.log('Contact saved ok!');
+            $('#success').text('Contacts imported succefuly: ' + contactsImported);
           };
 
           request.onerror = function onerror() {
+            contactsError++;
             console.log('ERROR on saving contact!');
+            $('#error').text('Contacts with errors: ' + contactsError);
           };
         }
 
-        
       };
-
-       
     };
+
--- js/vCard.js
+++ js/vCard.js
@@ -5,6 +5,9 @@
 //
 //                      Extended by Sebastián Rajo - @elecay - May/2013
 
+// Adding Javascript 1.6 Compatibility
+if(!Array.prototype.forEach){Array.prototype.forEach=function(d,c){c=c||this;for(var b=0,a=this.length;b<a;b++){d.call(c,this[b],b,this)}}}if(typeof Prototype!="undefined"||!Array.prototype.map){Array.prototype.map=function(d,c){c=c||this;var e=[];for(var b=0,a=this.length;b<a;b++){e.push(d.call(c,this[b],b,this))}return e}}if(typeof Prototype!="undefined"||!Array.prototype.filter){Array.prototype.filter=function(d,c){c=c||this;var e=[];for(var b=0,a=this.length;b<a;b++){if(d.call(c,this[b],b,this)){e.push(this[b])}}return e}}["forEach","map","filter","slice","concat"].forEach(function(a){if(!Array[a]){Array[a]=function(b){return this.prototype[a].apply(b,Array.prototype.slice.call(arguments,1))}}});Date.ISO8601PartMap={Year:1,Month:3,Date:5,Hours:7,Minutes:8,Seconds:9};Date.matchISO8601=function(a){return a.match(/^(\d{4})(-?(\d{2}))?(-?(\d{2}))?(T(\d{2}):?(\d{2})(:?(\d{2}))?)?(Z?(([+\-])(\d{2}):?(\d{2})))?$/)};Date.parseISO8601=function(e){var b=this.matchISO8601(e);if(b){var a=new Date,c,d=0;for(var f in this.ISO8601PartMap){if(part=b[this.ISO8601PartMap[f]]){a["set"+f]((f=="Month")?parseInt(part)-1:parseInt(part))}else{a["set"+f]((f=="Date")?1:0)}}if(b[11]){d=(parseInt(b[14])*60)+parseInt(b[15]);d*=((parseInt[13]=="-")?1:-1)}d-=a.getTimezoneOffset();a.setTime(a.getTime()+(d*60*1000));return a}};
+
 
 vCard = {
   initialize: function(_input){
@@ -59,10 +62,10 @@ vCard = {
         
         properties = results[2].split(';');
         properties = Array.filter(properties, function(p) { return ! p.match(/[a-z]+=[a-z]+/) });
-        properties = Array.map(properties, function(p) { return p.replace(/type=/g, '') });
+        properties = Array.map(properties, function(p) { return p.replace(/TYPE=/g, '') });
         
         type = properties.pop() || 'default';
-        type = type.toLowerCase();
+        type = type.toUpperCase();
         
         value = results[3];
         value = /;/.test(value) ? [value.split(';')] : value;
