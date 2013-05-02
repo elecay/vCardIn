b2ec7b74ea2a5bfc920933ba6021f632fb5b047c
elecay <elecay@gmail.com>
2013-05-02 18:43:59 +0200

    Added graphic loader. Engine almost done. Looking forward with the design.

--- js/engine.js
+++ js/engine.js
@@ -1,124 +1,170 @@
+/**
+* vCard Contacts Importer for FirefoxOS
+*
+* Created by Sebastián Rajo, May 2013.
+* Released under the MIT License.
+*
+* References:
+* 
+*   -Mozilla FirefoxOS WebAPI/ContactsAPI (https://wiki.mozilla.org/WebAPI/ContactsAPI)
+*/
 
-    contactsImported = 0;
-    contactsError = 0;
+(function () {
 
-    var storage = navigator.getDeviceStorage("sdcard"), 
-    cursor = storage.get('contacts.vcf'); 
+    DEBUG_MODE = true;
+    SDCARD = "sdcard";
+    CONTACTS_FILE_NAME = "contacts.vcf";
 
-    cursor.onerror = function() {
-        console.error("Error in: ", cursor.error.name);
-    };
+    /*
+     * According to RFC-2426 (http://www.ietf.org/rfc/rfc2426.txt)
+     */
+    POST_OFFICE_BOX = 0;
+    EXTENDED_ADDRESS = 1;
+    STREET_ADDRESS = 2;
+    LOCALITY = 3;
+    REGION = 4;
+    POSTAL_CODE = 5;
+    COUNTRY_NAME = 6;
 
-    cursor.onsuccess = function() { 
-      var file = cursor.result;
-      oFReader = new FileReader();
-      oFReader.readAsText(file);
+    if(DEBUG_MODE) {
+      // START CLEANING
+      req = navigator.mozContacts.clear();
+      req.onsuccess = function(){
+        console.log("Contact DB clean");
+      }
+      req.onerror = function(){
+        console.log("Error on delete contacts DB: " + req.error.name);
+      }
+      // END CLEANING
+    }
 
-      oFReader.onload = function (oFREvent) {
-        var arr = vCard.initialize(oFREvent.target.result);
-
-        for (var i = 0; i < arr.length; i++){
-          var contact = new mozContact();
-          var person = arr[i];
-
-          var emails = [];
-          var addresses = [];
-          var tels = [];
-          var urls = [];
-
-          for(type in person.email) {
-            for(n in person.email[type]) {
-              value = person.email[type][n];
-              email = { type: type, value: value };
-              emails.push(email);
-            }
-          }
-
-          for(type in person.tel) {
-            for(n in person.tel[type]) {
-              value = person.tel[type][n];
-              tel = { type: type, value: value };
-              tels.push(tel);
-            }
-          }
-          /*
-          * According to RFC-2426 (http://www.ietf.org/rfc/rfc2426.txt)
-          */
-          var POST_OFFICE_BOX = 0;
-          var EXTENDED_ADDRESS = 1;
-          var STREET_ADDRESS = 2;
-          var LOCALITY = 3;
-          var REGION = 4;
-          var POSTAL_CODE = 5;
-          var COUNTRY_NAME = 6;
-
-          for(type in person.adr) {
-            for(n in person.adr[type]) {
-              value = person.adr[type][n];
-              address = {
-                type: type,
-                streetAddress: value[STREET_ADDRESS],
-                locality: value[LOCALITY],
-                region: value[REGION],
-                postalCode: value[POSTAL_CODE],
-                countryName: value[COUNTRY_NAME]
-              };
-              addresses.push(address);
-            }
-          }
-
-          for(type in person.url) {
-            for(n in person.url[type]) {
-              value = person.url[type][n];
-              url = { type: type, value: value };
-              urls.push(url);
-            }
-          }
-
-          // leaving just the last value as last name by default
-          names = person.fn.split(' ');
-          n = "";
-          ln = "";
-          if (names.length > 1) {
-            for (var j = 0; j < names.length - 1; j++){
-              n += names[i] + " ";
-            }
-            ln = names[names.length - 1];
-          } else {
-            n = names[0] + " ";
-          }
-
-          contact.init(
-            {
-              name: person.fn,
-              givenName: n.substring(0, n.length - 1),
-              familyName: ln,
-              email: emails,
-              url: urls,
-              tel: tels,
-              adr: addresses,
-              org: person.org,
-              photo: person.photo,
-              jobTitle: person.title,
-              bday: person.bday
-            }
-          );
-
-          var request = navigator.mozContacts.save(contact);
-
-          request.onsuccess = function onsuccess() {
-            contactsImported++;
-            console.log('Contact saved ok!');
-            $('#success').text('Contacts imported succefuly: ' + contactsImported);
-          };
-
-          request.onerror = function onerror() {
-            contactsError++;
-            console.log('ERROR on saving contact!');
-            $('#error').text('Contacts with errors: ' + contactsError);
-          };
+    function extract(person_attribute){
+      var response = [];
+      for(type in person_attribute) {
+        for(n in person_attribute[type]) {
+          value = person_attribute[type][n];
+          pref = /pref/i.test(type);
+          var t = type.split(",")[0];
+          // Improve this!!!
+          if(t.toUpperCase() == 'INTERNET' || t.toUpperCase() == 'PREF')
+            t = "personal";
+          element = { type: t.toLowerCase(), value: value, pref: pref };
+          response.push(element);
         }
+      }
+      return response;
+    }
 
-      };
-    };
+    storage = navigator.getDeviceStorage(SDCARD), 
+      contacts_file = storage.get(CONTACTS_FILE_NAME); 
 
+    loader = $("#loading").percentageLoader();
+    loader.setValue(0);
+    loader.setProgress(0.0);
+
+    // Importer
+    var importer = document.querySelector("#import");
+    if (importer) {
+        importer.onclick = function() {
+
+          storage = navigator.getDeviceStorage(SDCARD), 
+            contacts_file = storage.get(CONTACTS_FILE_NAME); 
+
+          contactsImported = 0;
+          contactsError = 0;
+          totalToAdd = 0;
+
+          contacts_file.onerror = function() {
+            console.error("Error in: ", contacts_file.error.name);
+          };
+
+          contacts_file.onsuccess = function() { 
+            var file = contacts_file.result;
+            oFReader = new FileReader();
+            oFReader.readAsText(file);
+
+            oFReader.onload = function (oFREvent) {
+              iteration = 0;
+              var arr = vCard.initialize(oFREvent.target.result);
+              totalToAdd = arr.length;
+
+              for (var i = 0; i < totalToAdd; i++) {
+                var contact = new mozContact();
+                var person = arr[i];
+
+                var addresses = [];
+                var emails = extract(person.email);
+                var tels = extract(person.tel);
+                var urls = extract(person.url);
+
+                for(type in person.adr) {
+                  for(n in person.adr[type]) {
+                    value = person.adr[type][n];
+                    pref = /pref/i.test(type);
+                    var t = type.split(",")[0];
+                    // Improve this!!!
+                    if(t.toUpperCase() == 'INTERNET' || t.toUpperCase() == 'PREF')
+                      t = "personal";
+                    address = {
+                      type: t.toLowerCase(),
+                      streetAddress: value[STREET_ADDRESS],
+                      locality: value[LOCALITY],
+                      region: value[REGION],
+                      postalCode: value[POSTAL_CODE],
+                      countryName: value[COUNTRY_NAME],
+                      pref: pref
+                    };
+                    addresses.push(address);
+                  }
+                }
+
+                // leaving just the last value as last name by default
+                person.fn = (person.fn == null) ? "" : person.fn;
+                names = person.fn.split(" ");
+                n = "";
+                ln = "";
+                if (names.length > 1) {
+                  for (var j = 0; j < names.length - 1; j++){
+                    n += names[j] + " ";
+                  }
+                  ln = names[names.length - 1];
+                }
+                else {
+                  n = names[0] + " ";
+                }
+
+                contact.init(
+                  {
+                    name: person.fn,
+                    givenName: n.substring(0, n.length - 1),
+                    familyName: ln,
+                    email: emails,
+                    url: urls,
+                    tel: tels,
+                    adr: addresses,
+                    org: person.org,
+                    photo: person.photo,
+                    jobTitle: person.title,
+                    bday: person.bday
+                  }
+                );
+
+                var request = navigator.mozContacts.save(contact);
+
+                request.onsuccess = function() {
+                  contactsImported++;
+                  var times = (contactsImported / totalToAdd).toFixed(2);
+                  loader.setValue(contactsImported);
+                  loader.setProgress(times);
+                };
+
+                request.onerror = function() {
+                  contactsError++;
+                  console.log("ERROR on saving contact: " + request.error.name);
+                };
+              }
+            }
+          };
+        };
+    }
+})();
--- js/vCard.js
+++ js/vCard.js
@@ -8,7 +8,6 @@
 // Adding Javascript 1.6 Compatibility
 if(!Array.prototype.forEach){Array.prototype.forEach=function(d,c){c=c||this;for(var b=0,a=this.length;b<a;b++){d.call(c,this[b],b,this)}}}if(typeof Prototype!="undefined"||!Array.prototype.map){Array.prototype.map=function(d,c){c=c||this;var e=[];for(var b=0,a=this.length;b<a;b++){e.push(d.call(c,this[b],b,this))}return e}}if(typeof Prototype!="undefined"||!Array.prototype.filter){Array.prototype.filter=function(d,c){c=c||this;var e=[];for(var b=0,a=this.length;b<a;b++){if(d.call(c,this[b],b,this)){e.push(this[b])}}return e}}["forEach","map","filter","slice","concat"].forEach(function(a){if(!Array[a]){Array[a]=function(b){return this.prototype[a].apply(b,Array.prototype.slice.call(arguments,1))}}});Date.ISO8601PartMap={Year:1,Month:3,Date:5,Hours:7,Minutes:8,Seconds:9};Date.matchISO8601=function(a){return a.match(/^(\d{4})(-?(\d{2}))?(-?(\d{2}))?(T(\d{2}):?(\d{2})(:?(\d{2}))?)?(Z?(([+\-])(\d{2}):?(\d{2})))?$/)};Date.parseISO8601=function(e){var b=this.matchISO8601(e);if(b){var a=new Date,c,d=0;for(var f in this.ISO8601PartMap){if(part=b[this.ISO8601PartMap[f]]){a["set"+f]((f=="Month")?parseInt(part)-1:parseInt(part))}else{a["set"+f]((f=="Date")?1:0)}}if(b[11]){d=(parseInt(b[14])*60)+parseInt(b[15]);d*=((parseInt[13]=="-")?1:-1)}d-=a.getTimezoneOffset();a.setTime(a.getTime()+(d*60*1000));return a}};
 
-
 vCard = {
   initialize: function(_input){
     var vc = {};
@@ -36,33 +35,28 @@ vCard = {
     for (n in lines) {
       line = lines[n];
 
-      if(regexps['start'].test(line))
-      {
+      if(regexps['start'].test(line)) {
         fields = {};
       }
 
-      else if(regexps['end'].test(line))
-      {
+      else if(regexps['end'].test(line)) {
         res_array.push(fields);
       }
       
-      else if(regexps['simple'].test(line))
-      {
+      else if(regexps['simple'].test(line)) {
         results = line.match(regexps['simple']);
         key = results[1].toLowerCase();
         value = results[2];
-        
         fields[key] = value;
       }
       
-      else if(regexps['complex'].test(line))
-      {
+      else if(regexps['complex'].test(line)) {
         results = line.match(regexps['complex']);
         key = results[1].replace(regexps['key'], '').toLowerCase();
         
         properties = results[2].split(';');
         properties = Array.filter(properties, function(p) { return ! p.match(/[a-z]+=[a-z]+/) });
-        properties = Array.map(properties, function(p) { return p.replace(/TYPE=/g, '') });
+        properties = Array.map(properties, function(p) { return p.replace(/TYPE=/ig, '') });
         
         type = properties.pop() || 'default';
         type = type.toUpperCase();
@@ -74,97 +68,6 @@ vCard = {
         fields[key][type] = fields[key][type] || [];
         fields[key][type] = fields[key][type].concat(value);
       }
-    }
-  },
-  SingletonMethods: {
-    to_html: function() {
-      var output = '<div class="vcard">';
-      
-      if(this.photo)
-      {
-        output += '<img class="photo" src="data:image/png;base64,' + this.photo['base64'][0] + '" />';
-      }
-      
-      output += '<span class="fn">' + this.fn + '</span>'; // Required
-      
-      if(this.title)
-      {
-        output += '<span class="title">' + this.title + '</span>';
-      }
-      
-      if(this.org)
-      {
-        output += '<span class="org">' + this.org + '</span>';
-      }
-
-      output += '<hr/>'
-      
-      for(type in this.adr)
-      {
-        for(n in this.adr[type])
-        {
-          value = this.adr[type][n];
-          
-          output += '<address class="adr">';
-          output += '<span class="type">' + type + '</span>';
-          output += '<div class="content">';
-        
-          adr_fields = ['post-office-box', 'extended-address', 'street-address', 
-                        'locality', 'region', 'postal-code', 'country-name'       ]
-          for(field in adr_fields)
-          {
-            if(value[field])
-            {      
-              output += '<span class="' + adr_fields[field] + '">';
-              output += value[field];
-              output += '</span>';
-            }
-          }
-        
-          output += '</div>';
-          output += '</address>';
-        }
-      }
-      
-      for(type in this.tel)
-      {
-        for(n in this.tel[type])
-        {
-          value = this.tel[type][n];
-          output += '<span class="tel">';
-          output += '<span class="type">' + type + '</span>';
-          output += '<span class="value">' + value + '</span>';
-          output += '</span>';
-        }
-      }
-      
-      for(type in this.email)
-      {
-        for(n in this.email[type])
-        {
-          value = this.email[type][n];
-          output += '<span class="email">';
-          output += '<span class="type">' + type + '</span>';
-          output += '<a class="value" href="mailto:' + value + '">' + value + '</a>';
-          output += '</span>';
-        }
-      }
-      
-      for(type in this.url)
-      {
-        for(n in this.url[type])
-        {
-          value = this.url[type][n];
-          output += '<span class="url">';
-          output += '<span class="type">' + type + '</span>';
-          output += '<a class="value" href="' + value + '">' + value + '</a>';
-          output += '</span>';
-        }
-      }
-      
-      output += '</div>';
-      output = output.replace(/\\n/g, '<br/>');
-      return output;
     }
   },
   extend : function(dest, source) {
