b99dc1b59d78742efbe06cf3de8ca709889d232e
elecay <elecay@gmail.com>
2013-05-03 02:15:18 +0200

    Cleaning up

--- contacts/test/unit/mock_contacts_settings.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var MockContactsSettings = {
-  init: function() {},
-  close: function() {},
-  refresh: function() {},
-  onLineChanged: function() {},
-  cardStateChanged: function() {}
-};
--- contacts/test/unit/mock_l10n.js
+++
@@ -1,7 +0,0 @@
-window.realL10n = window.navigator.mozL10n;
-
-window.navigator.mozL10n = {
-  get: function get(key) {
-    return key;
-  }
-};
